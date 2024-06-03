beed938adb63825cf14394c8cb3db56e60d5a7b1
Eric Pettersson <eric.pettersson1@gmail.com>
2024-06-03 23:21:06 +0200

    feat: update root layout

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,16 @@
+import { Background } from "@components/background";
+import { Header } from "@components/header";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <Header />
+      <Background />
+      <div className="container mx-auto px-2 md:px-4">
+        <Outlet />
+      </div>
+    </>
   );
 };
 
