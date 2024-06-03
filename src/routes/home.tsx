adc4b6059395b60983abf1b6daf63a24bdd78295
Eric Pettersson <eric.pettersson1@gmail.com>
2024-06-03 23:17:04 +0200

    feat: add search

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,5 +1,13 @@
+import { ShowSearch } from "@components/show-search";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="relative left-1/2 top-[20vh] w-full -translate-x-1/2 -translate-y-1/4 sm:max-w-2xl">
+        <ShowSearch />
+      </div>
+    </>
+  );
 };
 
 export { Home };
